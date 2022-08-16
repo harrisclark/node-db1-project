6d4c43bf15a99b1a03d2a9358441a0e052586c6f
Andrew Clark <andrewclark@Andrews-MacBook-Pro-2.local>
2022-08-15 22:09:46 -0500

    passing mvp

--- api/server.js
+++ api/server.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const server = express();
 
+const accountsRouter = require('./accounts/accounts-router');
+
 server.use(express.json());
+
+server.use('/api/accounts', accountsRouter);
+
+server.use('*', (req, res) => {
+  res.status(404).json({ message: 'invalid path' })
+});
+
+// server.use((err, req, res, next) => {
+//   const { status = 500, message = "something went wrong with the server" } = err;
+//   res.status(status).json({ message: message })
+// })
 
 module.exports = server;
